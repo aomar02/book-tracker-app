0b9dba0622aa1190b86581fb18f1b1acb7f7deaf
aomar02 <aaljabri.002@gmail.com>
2025-07-28 13:52:32 +0800

    vercel default route

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard', // or '/auth/login'
+        permanent: true,
+      }
+    ]
+  }
+}
+
 export default nextConfig;
